96462a405df1d8553c496f822f5104d2d5c06f5c
toshi120 <toshizy@gmail.com>
2021-05-23 20:09:35 +0900

    先生用マイページ遷移リンク表示のしゅうせい

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,20 @@
 class StudentsController < ApplicationController
-before_action :block_access, only: [:show]
-
+before_action :set_student  
+before_action :block_access
   def show
-    student = Student.find(params[:id])
-    @name = student.name
-    @my_textbooks = StudentsTextbook.where(student_id: student.id)
+    @name = @student.name
+    @my_textbooks = StudentsTextbook.where(student_id: @student.id)
   end
 
   private
 
   def block_access
-    if current_student != student
+    if current_student != @student
       redirect_to students_textbooks_path
     end
+  end
+
+  def set_student
+    @student= Student.find(params[:id])
   end
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,8 @@
 class TeachersController < ApplicationController
 
   def show
-    @teacher_textbook = Textbook.
+    teacher = Teacher.find(params[:id])
+    @name = Teacher.name
+    @my_textbooks = Textbook.where(teacher_id: teacher.id)
+  end
 end
