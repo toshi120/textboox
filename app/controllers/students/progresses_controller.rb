85458cfb2f5329d7db580be16502aa16698c9b5b
toshi120 <toshizy@gmail.com>
2021-06-19 18:09:10 +0900

    progressモデルバリデーション設定

--- app/controllers/students/progresses_controller.rb
+++ app/controllers/students/progresses_controller.rb
@@ -1,22 +1,27 @@
 class Students::ProgressesController < Students::ApplicationController
+  before_action :set_textbook
+
 
   def new
-    @textbook = Textbook.find(params[:textbook_id])
+   
     @students_textbook = StudentsTextbook.where(student_id: current_student.id).find_by(textbook_id: @textbook.id)  
     @progress = Progress.new
   end
 
   def create
-    @textbook = Textbook.find(params[:textbook_id]) 
+    
     @progress = Progress.new(progress_params)
     if @progress.save
-      redirect_to students_textbooks_path(@textbook)
+      redirect_to students_textbook_path(@textbook)
     else
       render :new
     end
   end
 
   private
+  def set_textbook
+    @textbook = Textbook.find(params[:textbook_id])
+  end
 
   def progress_params
     params.require(:progress).permit(:progress_page, :comment).merge(students_textbook_id: params[:students_textbook_id])
