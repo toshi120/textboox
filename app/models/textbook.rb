5eb00c74077629d6022e079f017419c690bd5f52
toshi120 <toshizy@gmail.com>
2021-06-19 16:00:34 +0900

    textbookモデルのバリデーション追加

--- app/models/textbook.rb
+++ app/models/textbook.rb
@@ -3,5 +3,6 @@ class Textbook < ApplicationRecord
   has_many :students_textbooks, foreign_key: :textbook_id, dependent: :destroy
   has_many :students, through: :students_textbooks
 
-  validates :image, presence: true
+  validates :name, :note, :page, :image, presence: true
+  validates :page, numericality: { less_than_or_equal_to: 999_999 }
 end
