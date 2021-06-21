5f8ff99c1b97b2227644d7ad89239e316bcf4655
toshi120 <toshizy@gmail.com>
2021-06-21 17:54:04 +0900

    不要なコメントアウト削除

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,7 +1,7 @@
 class Progress < ApplicationRecord
   belongs_to :students_textbook, dependent: :destroy
 
-  validates :progress_page, :comment, presence: true
-  
+  validates :progress_page, presence: true
+  validates :comment, presence: true
   #progress_pageにのマックスの数字をその参考書の総ページにするバリデーションを作成する
 end
