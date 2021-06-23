a0a973741c0b4da8f9439ee386a2216415fca9ba
toshi120 <toshizy@gmail.com>
2021-06-23 17:39:24 +0900

    comment.jsのコメントアウト修正

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -7,22 +7,13 @@ const pullDownComment = document.getElementsByClassName("progresses-list");
 const pullDownComments = Array.from(pullDownComment);
 console.log(pullDownComments);
 
-// let x = 0
-// pullDownNames[x].addEventListener("click", function() {
-//   pullDownComments[x].setAttribute("style", "display:block");
-// })
-
-// pullDownNames[1].addEventListener("click", function() {
-//   pullDownComments[1].setAttribute("style", "display:block");
-// })
-
 for (let indexNum = 0; indexNum < pullDownNames.length; indexNum++) {
   pullDownNames[indexNum].addEventListener("click", function() {
     if (pullDownComments[indexNum].getAttribute("style") == "display:block;") {
       pullDownComments[indexNum].removeAttribute("style", "display:block;")      
     } else {
       debugger
-    pullDownComments[indexNum].setAttribute("style", "display:block")
+    pullDownComments[indexNum].setAttribute("style", "display:block;")
     }
   });
 }
