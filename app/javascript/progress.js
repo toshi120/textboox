4e2d12f441352b8dcd45503f12c67f92e604878c
toshi120 <toshizy@gmail.com>
2021-06-27 09:36:00 +0900

    進捗フォームを表示

--- app/javascript/progress.js
+++ app/javascript/progress.js
@@ -1,15 +1,17 @@
+const { $ } = require("@rails/ujs");
+
 function progressDisplay() {
 
-console.log("イベント発火")
+const progressBtn = document.querySelector('.progress-btn');
+const progressForm = document.querySelector('.progress-form-box');
 
-
-
-
-
-
-
-
-
+progressBtn.addEventListener('click', function() {
+  if (progressForm.getAttribute("style") == "display:block;") {
+    progressForm.removeAttribute("style", "display:block;")      
+  } else {
+    progressForm.setAttribute("style", "display:block;")
+  };
+})
 
 
 
