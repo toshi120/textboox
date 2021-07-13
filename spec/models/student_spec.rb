9dc20a23735d07012c7f17e03f8df1c3ec5fe7aa
toshi120 <toshizy@gmail.com>
2021-07-13 16:43:24 +0900

    student factorybot正常に確認

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @student = FactoryBot.build(:student)
+  end
+
+  describe '生徒ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it '全ての項目が存在すれば登録できる' do
+        expect(@student).to be_valid
+      end
+    end
+  end
 end
