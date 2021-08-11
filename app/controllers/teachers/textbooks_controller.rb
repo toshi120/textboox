9bb4e68526c23ffe6628de811a32304951046a40
toshi120 <toshizy@gmail.com>
2021-08-11 11:13:38 +0900

    検索機能

--- app/controllers/teachers/textbooks_controller.rb
+++ app/controllers/teachers/textbooks_controller.rb
@@ -71,13 +71,11 @@ class Teachers::TextbooksController < Teachers::ApplicationController
     end
   
 
-
     @students_textbook.each do | x |
       if @students_id.include?(x.student.id)
         @studying_students << x
       end
     end
-    binding.pry
 
   end
 
